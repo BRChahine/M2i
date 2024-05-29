4c68c8ae7a5ee532428fcad5dd00faa27156b239
BenRabiaaChahine <benrabiaachahine@gmail.com>
2024-05-29 10:53:03 +0200

    first commit

---
+++ js/jscours.js
@@ -0,0 +1,243 @@
+// Les commentaires
+/*Les commentaires 
+multilignes*/
+
+/* let nom = "Pierre"; // type string
+let age = 65;       // type number => int
+let money = 675.98; // type number => float
+let vrai = true;    // type boolean
+let x = undefined;
+
+console.log(nom);
+console.log(age);
+console.log(money);
+console.log(vrai);
+console.log(x); */
+
+/* let nom = "Pierre";
+nom = "Jean";
+
+const age = 45;
+
+var prenom = "Jackie";
+prenom = "Max"
+var prenom = "Viviane"
+console.log(prenom)
+console.log(nom);
+console.log(age); */
+
+//Operateurs arithmetique
+
+/* let x = 25;
+let y = 30;
+
+console.log(x + y);
+console.log(x - y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+ */
+
+/* let x = 5 , y = 10 , z = -2;
+    let priorite  = x + y / (4 + z ) % 3;
+ */
+
+/* let x = 5;
+x = x + 5; 
+x += 5;
+console.log(x) */
+
+/* let departement =  'je vis dans le "38" ' ;
+console.log(departement) */
+
+/* let prenom =  "Pierre";
+
+let nom = "Dupont";
+
+let numberHome = 45;
+
+let street = "Boulevard Victor Hugo";
+
+let person = ` Monsieur ${prenom} ${nom} habite au ${numberHome} de la rue ${street} `;
+
+console.log(person); */
+
+/* let hour = 10;
+    if (hour <= 10){
+        console.log("Oui il est inferieur ou egal à 10")
+    }
+    else{
+        console.log("Non il n'est pas inferieur ou egal à 10")
+    } */
+
+/* let hour = 12;
+    if ( hour <= 12 ){
+        console.log("Bonjour")
+    } 
+    else if(hour >12 && hour <= 18){
+        console.log("Bonne après-midi")
+    }   
+    else {
+        console.log("Bonsoir")
+    }
+ */
+
+/*  //switch 
+    let heure = 10;
+     
+    switch (heure){
+        case 8:
+            console.log("il est 8 heures");
+            break;
+        case 9:
+            console.log("il est 9 heures");
+            break;
+        case 10:
+            console.log("il est 10 heures");
+            break;   
+        case 11:
+            console.log("il est 11 heures");
+            break;   
+        case 12:
+            console.log("il est 12 heures");
+            break; 
+        default:
+            console.log("rien à afficher");   
+
+    }
+ */
+//let heure = 12;
+/*  
+ if (heure < 11){
+    console.log("Bonjour");   
+ }
+ else{
+    console.log("Bonsoir");
+ } */
+
+/*  let bonneHeure;
+
+ bonneHeure = (heure<11) ? "Bonjour" : "Bonsoir";
+
+ console.log(bonneHeure);
+ */
+
+/*  let x = 0;
+
+ while ( x < 10 ){
+    console.log('x contient la valeur:' + x);
+    x++;
+ } */
+
+/*  let y = 0;
+
+ do{
+    console.log('y vaut : ' + y);
+    y++;
+ } while (y<10) */
+
+ /* for (let i = 0 ; i < 10; i++){
+    console.log("i contient la valeur:" +i)
+ } */
+
+ /* //exo 1
+  let title = "Top 10 des meilleurs vehicules de France ";
+  title = "Top 5 des meilleurs motos de France";
+ 
+
+//exo 2
+const money = 5000; 
+
+//exo 3
+let temperature;
+temperature = 20;
+
+//exo 4
+  
+
+        
+                LET UsErNaM = "John"; //faux
+                const _id = 99;  //vrai
+                let class = true; //faux
+                const isAdult = true. //vrai
+                const $card = {name: "crd", content: "lorem ipsum"}; //faux
+                let 1452 = 1452; //faux
+                const sum41 = "music"; //vrai
+
+  
+
+//exo 5
+
+const squarefeet = 100;
+
+const size = squarefeet > 70 ? "large" : "Small/Medium"
+console.log(size)
+
+
+//exo 6
+
+let tab = [7,4,86,"Pierre",29,"Jean",35,15,65,"Arthur"];
+for (let i = 0 ; i < 10; i++){
+    console.log(tab[i]);
+}
+
+//exo 7
+
+let choix = 10;
+
+    switch(choix){
+        case 1 :
+            console.log("Chat");
+            break;
+        case 2 :
+            console.log("Chien");
+            break;
+        case 3 :
+            console.log("Oiseau");
+            break;
+        case 4 :
+            console.log("Poisson");
+            break;
+        case 5 :
+            console.log("Giraffe");
+            break;
+        case 6 :
+            console.log("Crocodile");
+            break;  
+        case 7 :
+            console.log("Elephant");
+            break; 
+        case 8 :
+            console.log("Dromadaire");
+            break; 
+        default :
+            console.log("Serpent");
+}
+
+//exo 8
+
+let car = "Ferrari";
+let color = "rouge";
+let userName = "Christophe";
+let price = 45000;
+let timing = " il y a une semaine";
+let achat = ` ${timing} Monsieur ${userName} a acheté une ${car} de couleur ${color} pour un prix de ${price} `;
+console.log(achat);
+
+//exo 9
+
+let isLogged = true;
+if (isLogged){
+    console.log("Welcome");
+}
+else{
+    console.log("Access denied");
+}
+
+//exo 10
+
+let result = 30;
+result *= 6
+console.log(result);
+ */
---
+++ js/m2i.js
@@ -0,0 +1,207 @@
+/* const animaux = ["chat" , "chien" , "oiseaux" , "poules" , "canard", "lion" , "tortue"];
+animaux[0] = "souris"
+
+// Methode push
+animaux.push("serpent");
+
+// Methode pop
+animaux.pop();
+
+// Methode unshift
+animaux.unshift("pigeon");
+
+// Methode shift
+animaux.shift(); */
+
+// iteration dans les tableaux
+
+/* for[i]
+    for(let i = 0; i < animaux.length; i++){
+    console.log(animaux[i])
+}
+
+ */
+
+/* for of
+ for (let animal of animaux ){
+    console.log(animal)
+} */
+
+/* // foreach
+animaux.forEach((animal, index) =>{
+    console.log(index,animal);
+}) */
+
+// Tableau
+
+/* const arr = ["Jean", "Victor", ["Tony",8,22], "Xavier"];
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+} */
+
+/* const tab = [["Sarrah", "Tania"], ["Rose","Viviane"], ["Eric","Rudolph"], ["Marc", "Andre"]];
+
+for(let i = 0; i < tab.length; i++){
+    for(let j = 0; j < tab[i].length; j++){
+        console.log(tab[i][j]);
+    }
+}
+
+for (const name of tab){
+    for(const surname of name){
+        console.log(surname)
+    }
+}
+//console.log(tab) */
+
+// .map = retourne un tableau
+/* 
+const isNumber = [2,4,6,8,10];
+isNumber.map(num =>{
+    console.log(num * 2);
+}) */
+
+/* // Methode .filter
+const heights = [160, 175, 184,193,205,237,365,386,499];
+const above205 = heights.filter(height =>{
+    return height > 205;
+})
+console.log(above205); */
+
+//const animaux = ["chat" , "chien" , "oiseaux" , "poules" , "canard", "lion" , "tortue"];
+//console.log(animaux.sort().reverse());
+//IndexOf
+//console.log(animaux.indexOf("poules"))
+//Includes
+//console.log(animaux.includes("chat"))
+
+/* const numbers2 = [75,23,95,16,59,65,96];
+const mergeTab = numbers.concat(numbers2);
+console.log(mergeTab);
+//console.log(numbers.sort().reverse())
+ */
+// Methode flat
+/* const numbers = [55,73,12,94,[75,[23,95,16,59],65,96],98,21,43];
+console.log(numbers.flat(2));
+ */
+
+/* //Methode split
+let string = "abcd";
+console.log(string.split(""))
+
+//methode join
+let stringTab = ["y", "v", "z"];
+console.log(stringTab.join(""))
+
+//Methode slice
+const animaux = ["chat" , "chien" , "oiseaux" , "poules" , "canard", "lion" , "tortue"];
+console.log(animaux.slice(2,6)) */
+
+//exo 1
+
+const tab = ["Paul", "Jane", "Dupont", "Fred", "Elena"];
+tab.pop()
+tab.shift()
+
+//exo 2
+
+const numbers = [85,93,83,47,25,45,20,46,33,54,11];
+//console.log(numbers.sort() .reverse());
+
+//exo 3
+
+const tab2 = ["Paul", "Jane", "Dupont", "Fred", "Elena"];
+tab2[0] = "Marc"
+//console.log(tab2)
+
+
+//exo 4
+
+const values = ["","a","xavier",false,8,0,12,NaN,true];
+
+const vrai = values.filter(value => value);
+//console.log(vrai)
+
+//exo 5
+
+const names = ["Adrien", "paul", "Victor", "Alexandre", "Aurelie", "Antoine"];
+for (const surname of names){
+    if(surname[0]== "A" && surname.length >= 5){
+       // console.log(surname)
+    }
+}
+
+//exo 6
+const names2 = ["André", "Jean", "Victor", [18,27,45,29], "Andréa"];
+for (const surname2 of names2){
+    //console.log(surname2)
+}
+    for(let i = 0; i < names2.length; i++){
+    //console.log(names2[i])
+}
+
+//exo 7
+const names3 = ["André", "Jean", "Victor", "Andréa"];
+//console.log(names3.join("-"))
+
+// exo 8
+const isNumber = [4,32,7,8];
+isNumber.map(num =>{
+    //console.log(num + "€");
+})
+
+// exo 9 
+const tabM = ["f",5,["d",3,["a",0],["b",1],["c",2]],["e",4],"g",6]
+//console.log(tabM[2][3]);
+
+// exo 10 
+
+const names4 = ["Adrien", "paul", "Victor", "Alexandre", "Aurelie", "Antoine"];
+names4.forEach((nom,index)=>{
+   //console.log(index,nom);
+})
+
+/* function nom (arguments){
+    code
+} */
+
+/* function sayHello(){
+    console.log("Bonjour");
+}
+sayHello() */
+
+/* function sayHello(name){
+    console.log(`Bonjour ${name}`);
+}
+sayHello("John");
+sayHello("Fred"); */
+
+/* const sayHello = (name)=>{
+    console.log(`Bonjour ${name}`);
+}
+sayHello("Garry") 
+
+const calc = (a,b)=>{
+    return a + b;
+}
+
+console.log(calc(10,5));
+*/
+/* const major = (old)=>{
+    if (old < 18){
+        return "Mineur";
+    }
+    else{
+        return "Majeur";
+    }
+}
+console.log(major(20)); */
+
+/**********************DATE*************************** */
+
+/* const dateInfo = Date.now();
+console.log(dateInfo); */
+
+const dateInfo2 = new Date();
+//console.log(dateInfo2);
+console.log(dateInfo2.toLocaleDateString())
